1e2a48fae45c996430a5bafc71e1acb44b8aaa19
Samuel Woodbridge <s-bridge@live.co.uk>
2018-03-22 10:16:27 +0000

    [Tests] - Fixes the smoke test

--- src/__tests__/SmokeTest.ts
+++ src/__tests__/SmokeTest.ts
@@ -1,8 +1,24 @@
 import PropForms from "../PropForms";
 import TestUtils from "../../TestUtils";
+import { JSDOM } from "jsdom";
 
-beforeEach(TestUtils.setUp);
+let form: HTMLFormElement;
+
+beforeEach((done: DoneFn) => {
+    TestUtils.setUp().then((dom: JSDOM) => {
+        document.body.innerHTML = dom.serialize();
+        const element = document.getElementById("form");
+
+        if (element && element instanceof HTMLFormElement) {
+            form = element;
+        } else {
+            fail("Cannot find form markup to conduct test");
+        }
+
+        done();
+    });
+}, 2000);
 
 it("initialises without crashing", () => {
-    new PropForms("#form");
+    new PropForms(form);
 });
